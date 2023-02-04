2802d7465648dbb20d36185e77abaa1590c90d19
ANSHxTHAKUR <sengaransh094@gmail.com>
2023-02-04 23:31:57 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const notes = require("./data/notes");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("App is running");
+});
+
+app.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+
+  res.send(note);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`Server started on port ${port}`));
